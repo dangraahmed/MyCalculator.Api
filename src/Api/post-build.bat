d50eb4c832c49e713434a0ab48c8c627e13bc825
Ahmed Amin Dangra <dangra.ahmed@gmail.com>
2018-02-20 16:08:48 +0530

    Unwanted code remove.

--- src/Api/post-build.bat
+++ src/Api/post-build.bat
@@ -2,10 +2,6 @@ set OutputPath=%1
 set FullTargetFramework=%2
 set Configuration=%3
 
-echo %1
-echo %2
-echo %3
-
 dotnet publish-iis --publish-folder %OutputPath% --framework %FullTargetFramework%
 
 
