7afad6de36133e3c5e0bf506ab339bf5f420696c
Ahmed Amin Dangra <dangra.ahmed@gmail.com>
2018-02-22 18:02:27 +0530

    Hard coded path remove.

--- post-develop/code-coverage.bat
+++ post-develop/code-coverage.bat
@@ -1,15 +1,16 @@
 mkdir "..\test\ApiTest\CodeCoverage"
 
-C:\Users\ahmed.dangra\.nuget\packages\OpenCover\4.6.519\tools\OpenCover.Console.exe ^
+%userprofile%\.nuget\packages\OpenCover\4.6.519\tools\OpenCover.Console.exe ^
     -target:"%ProgramFiles%\dotnet\dotnet.exe" ^
     -targetargs:"test ..\test\ApiTest" ^
     -register:user ^
     -filter:"+[*]* -[xunit*]*" ^
-    -output:"..\test\ApiTest\CodeCoverage\ApiTest.xml"
+    -output:"..\test\ApiTest\CodeCoverage\ApiTest.xml"^
+    -oldStyle
 
-C:\Users\ahmed.dangra\.nuget\packages\ReportGenerator\3.1.2\tools\ReportGenerator.exe ^
+%userprofile%\.nuget\packages\ReportGenerator\3.1.2\tools\ReportGenerator.exe ^
     -reports:"..\test\ApiTest\CodeCoverage\ApiTest.xml" ^
     -targetdir:"..\test\ApiTest\CodeCoverage"
 
 start "report" "..\test\ApiTest\CodeCoverage\index.htm"
-rem cls
+cls
